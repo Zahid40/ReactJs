d7c54e319121522e34bcdf8faa28d7be49573c28
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-03-11 21:31:09 +0530

    110324

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { Tooltip, initTWE, Ripple, Collapse } from "tw-elements";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [height, setHeight] = useState(0);
+  const [Time, setTime] = useState(0);
+
+  const heightRef = useRef();
+
+  useEffect(() => {
+    heightRef.current.focus();
+  });
+  useEffect(() => {
+    initTWE({ Tooltip });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Navbar />
+      <Card />
+      <label htmlFor="height">Height</label>
+      <input
+        name="height"
+        id="height"
+        type="number"
+        ref={heightRef}
+        onChange={(e) => setHeight(parseInt(e.target.value))}
+      />
+
+      <button
+        onClick={() => {
+          let calTime = Math.sqrt((2 * height) / 9.8);
+          setTime(calTime);
+        }}
+      >
+        Clal
+      </button>
+
+      <h2>Output</h2>
+      <p>Time Taken : {Time}</p>
+
+      <div className="mt-16 flex justify-center">
+        <p className="text-lg">
+          Hover the link to see the
+          <a
+            href="#"
+            className="text-primary ps-1 transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+            data-twe-toggle="tooltip"
+            title="Hi! I'm tooltip"
+          >
+            tooltip
+          </a>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
